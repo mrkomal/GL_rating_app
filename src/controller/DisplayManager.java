1947a7e4f90a191bd700faa28ed7da8fd4a04247
mrkomal <gry2@vp.pl>
2020-10-16 12:06:03 +0200

    :clipboard: display manager sketch

--- src/controller/DisplayManager.java
+++ src/controller/DisplayManager.java
@@ -12,26 +12,33 @@ public class DisplayManager {
         dbHandler = new DBHandler();
     }
 
-    public String getInputData(String input) throws SQLException {
-        String output = "a";
-        int rate = dbHandler.getIceCreamID(input);
-        return output;
-    }
-    /*
-    @Override
-    public void addNotify() {
-        super.addNotify();
+    public void getFlavourInfo(String flavour) throws SQLException {
+        //check if flavour exists in DB
 
-        if(thread == null){
-            thread = new Thread(this,"AppThread");
-            thread.start();
-        }
+        //conditional statement: if flavour exists get other info from DB
+
+        //update interface variables with obtained data
+
+        //update comment with performed action
+
     }
 
-    @Override
-    public void run() {
-        System.out.print("Thread OK");
+    public void getCurrentRanking() throws SQLException {
+        //get data from DB
+
+        //check data length (if smaller than 3 then, add default text)
+
+        //update interface variables with obtained data
+
+        //update comment with performed action
+
     }
 
-     */
+    public void addNewFlavour() throws SQLException {
+        //create new row for each table with given data
+
+        //update comment with performed action
+
+    }
+
 }
